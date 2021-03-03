5a18baa6d5717ec27b1d8abf091ee3c7205926a4
noa-barsky <barskyn@mcmaster.ca>
2021-03-03 01:49:49 -0500

    styled the table, added gitignore

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,41 +1,17 @@
-import './App.css';
-import Square from './Square';
-import { Row } from 'reactstrap';
+import Row from './components/Row'
 
 function App() {
-
-  const squareInfo = [];
-
+  const rows = [];
   for (var i = 0; i < 100; i++) {
-    squareInfo.push('');
+    rows.push(<Row></Row>);
   }
 
   return (  
     <div className="App">
-      <h1 style={{textAlign: 'center'}}> The Million Dollar NFT </h1>
-      
+      <h1 className="pageHeader"> The Million Dollar NFT </h1>
       <br></br>
-      <div class="homepage" style={{ borderStyle: 'solid',  margin: 'auto', height:'1000px', width:'1000px', backgroundColor: '#FFFF'}}>        
-        <div className="gridContainer">
-          {squareInfo.map(() => {
-            return <Square></Square>
-          })}          
-         </div>
-
-
-      
-        {/* {squareInfo.map(() => {
-          return <Square style={{marginLeft:'40px'}}></Square>
-        })}
-        {squareInfo.map(() => {
-          return <Square style={{marginLeft:'30px'}}></Square>
-        })}
-        {squareInfo.map(() => {
-          return <Square style={{marginLeft:'20px'}}></Square>
-        })}
-        {squareInfo.map(() => {
-          return <Square style={{marginLeft:'10px'}}></Square>
-        })} */}
+      <div className="homePage">       
+        {rows}
       </div>
     </div>
   );
