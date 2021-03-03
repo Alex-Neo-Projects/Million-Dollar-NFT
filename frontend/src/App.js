42559d4d62294af9bcc3d61596790021e2b2429d
Alex Reyes <reyesruizalex@gmail.com>
2021-03-02 23:35:03 -0500

    sense of scale

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <h1 style={{textAlign: 'center'}}> The Million Dollar NFT </h1>
+      <div class="square" style={{margin: 'auto', height:'10px', width:'10px', backgroundColor: '#555'}}></div>
+      <br></br>
+      <div class="homepage" style={{  margin: 'auto', height:'1000px', width:'1000px', backgroundColor: '#555'}}></div>
     </div>
   );
 }
