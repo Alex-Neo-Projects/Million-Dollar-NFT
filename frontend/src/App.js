077e46d8861bbb6a0365b4ef1a918284eebe4b52
Alex Reyes <reyesruizalex@gmail.com>
2021-03-08 20:16:08 -0500

    merge canvas changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,30 @@
 import './App.css';
-import Row from './components/Row';
+import Canvas from './components/Canvas';
 
 function App() {
-  const rows = [];
-  
-  for (var i = 0; i < 100; i++) {
-    rows.push(<Row rowNum={i}></Row>);
-  }
-
   return (
-    <div className="container">
+    <div>
+     <div className="container">
       
-      <div className="header-collection">
-        <div className="header-logo">
-          <h1> The Million Dollar <strike>Homepage</strike> NFT </h1>
-        </div>
-        <div className="header-info"> 
-            <p>1,000,000 pixels | $x per pixel | Own a piece of NFT history!</p>
-        </div>
+       <div className="header-collection">
+         <div className="header-logo">
+           <h1> The Million Dollar <strike>Homepage</strike> NFT </h1>
+         </div>
+         <div className="header-info"> 
+             <p>1,000,000 pixels | Starting at $1 per pixel | Own a piece of NFT history!</p>
+         </div>
       </div>
 
-     {/* Navigation */}
-     <div className="nav-links">
-        <p>Click on an open spot to buy. Redeem your NFT to put your ad/image on it!</p>
-      </div>
+       <div className="nav-links">
+         <p>Click on an open spot to buy. Redeem your NFT to put your ad/image on it!</p>
+       </div> 
 
-      <div className="ad">
+     <div className="ad">
         <div className="ad-collection">
-          <h1>{rows}</h1>
+          <Canvas />
         </div>
       </div>
-
+      </div>
     </div>
   );
 }
